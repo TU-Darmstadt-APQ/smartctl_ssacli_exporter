705d8a57beff48da27518e6ca6bb51425fd7c3ac
Patrick Baus <patrick.baus@physik.tu-darmstadt.de>
2021-08-03 03:07:32 +0200

    Added rotational speed

--- parser/ssacli_physDisk.go
+++ parser/ssacli_physDisk.go
@@ -17,6 +17,7 @@ type SsacliPhysDiskData struct {
 	IntType   string
 	Size      string
 	BlockSize string
+	Speed     int16
 	Firmware  string
 	SN        string
 	WWID      string
@@ -58,6 +59,8 @@ func parseSsacliPhysDisk(s string) *SsacliPhysDisk {
 				tmp.Size = kv[1]
 			case "Logical/Physical Block Size":
 				tmp.BlockSize = kv[1]
+			case "Rotational Speed":
+				tmp.Speed = toINT16(kv[1])
 			case "Firmware Revision":
 				tmp.Firmware = kv[1]
 			case "WWID":
